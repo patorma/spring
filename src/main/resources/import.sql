f5d3aba1e64d5c998dcc10e304601ca2891117e0
patorma <pocontreras@ing.ucsc.cl>
2020-01-05 20:36:16 -0300

    se creo fichero para ingresar datos a la bd

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/*populate tabla clierntes*/
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','patorma@yahoo.com', '2019-12-30');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres',  'pablo@gmail.com', '2019-11-24');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Contreras', 'daniela_pu@gmail.com','2019-03-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alex', 'Fernandez','Alex@gmail.com','2019-05-06');
