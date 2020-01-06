bc4e33616b49ec7f0d36ab438d2583312cb1ca85
patorma <pocontreras@ing.ucsc.cl>
2020-01-06 18:57:33 -0300

    se implemento en el controlador buscar por id y save

--- src/main/java/com/patriciocontreras/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -39,7 +40,11 @@ public class Cliente implements Serializable {
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
 	
-	
+	//antes de persistir vamos a hacer la fecha automatica
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date(); 
+	}
 	
 	
 	/**
