036214b25e0275acd570c4fa67ed4a2668a667b1
patorma <pocontreras@ing.ucsc.cl>
2020-01-06 21:27:30 -0300

    se termino el controlador de clientes con los verbos put y delete de api rest

--- src/main/java/com/patriciocontreras/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,32 +18,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.patriciocontreras.springboot.backend.apirest.models.entity.Cliente;
 import com.patriciocontreras.springboot.backend.apirest.models.services.IClienteService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
-	
+
 	@Autowired
 	private IClienteService clienteService;
-	
+
 	@GetMapping("/clientes")
-	public List<Cliente> index(){
+	public List<Cliente> index() {
 		return clienteService.findAll();
 	}
-	
+
 	@GetMapping("/cleintes/{id}")
 	public Cliente show(@PathVariable Long id) {
 		return clienteService.findById(id);
-		
+
 	}
-	
+
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente create(@RequestBody Cliente cliente) {
-		
+
 		return clienteService.save(cliente);
 	}
+
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente,@PathVariable Long id) {
+		//obtenemos el cliente de la bd por Id
+		Cliente clienteActual = clienteService.findById(id);
+		//modificamos los datos del cliente actual con los datos del cliente que te envien
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setEmail(cliente.getEmail());
+
+		return clienteService.save(clienteActual);
+	}
 	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
 	
 
 }
