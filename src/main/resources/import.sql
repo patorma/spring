d4f623f636beac3b9da0462f916b905148a95f25
patorma <pocontrera@ing.ucsc.cl>
2020-04-12 21:15:06 -0400

    se agregaron mas clientes a la bd por el tema de paginacion

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,21 +4,21 @@ INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres', 
 INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15');
 INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Contreras', 'daniela_pu@gmail.com','2019-03-28')
 INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alex', 'Fernandez','Alex@gmail.com','2019-05-06');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','patorma@yahoo.com', '2019-12-30')
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres',  'pablo@gmail.com', '2019-11-24')
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15')
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Contreras', 'daniela_pu@gmail.com','2019-03-28');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alex', 'Fernandez','Alex@gmail.com','2019-05-06');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','patorma@yahoo.com', '2019-12-30');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres',  'pablo@gmail.com', '2019-11-24');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Contreras', 'daniela_pu@gmail.com','2019-03-28');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alex', 'Fernandez','Alex@gmail.com','2019-05-06');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','patorma@yahoo.com', '2019-12-30');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres',  'pablo@gmail.com', '2019-11-24');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Contreras', 'daniela_pu@gmail.com','2019-03-28');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alex', 'Fernandez','Alex@gmail.com','2019-05-06');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','patorma@yahoo.com', '2019-12-30');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo', 'Torres',  'pablo@gmail.com', '2019-11-24');
-INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Saul','Palma', 'saul_palma@gmail.com','2019-09-15');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Iris','Marlene','irisita_miel@gmail.com', '1990-12-30')
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Daniela', 'Torres',  'dani@gmail.com', '1981-11-24')
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Ivan','Palma', 'ivan_elterrible@gmail.com','1995-09-15')
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Juan', 'Ortiz', 'palito_agua@gmail.com','1969-03-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Fernando', 'Fernandez','feña@gmail.com','2019-05-06');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Contreras','mevoya@hotmail.com', '2019-12-30');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Maria', 'Cifuentes',  'merry@gmail.com', '2020-11-24');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pedro','Palma', 'terminator@gmail.com','2019-09-15');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Eduardo', 'Yañez', 'edu_20245@gmail.com','2019-03-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Alexia', 'Perez','Alexia_bellaka@gmail.com','2019-05-06');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Tamara','Torres','tamara_torres@yahoo.com', '2019-12-30');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Rodrigo', 'Ortiz', 'rodri@gmail.com', '2019-11-24');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Samuel','Carrasco', 'samuel@gmail.com','2019-09-15');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Sebastian', 'Contreras', 'seba@gmail.com','2019-03-28');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Leslie', 'Fernandez','Leslie_bella@gmail.com','2019-05-06');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Patricio','Suarez','hombre_audaz@yahoo.com', '2019-12-30');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Lorena', 'Suarez',  'lore@gmail.com', '2019-11-24');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Felicia','Palma', 'feliz@gmail.com','2019-09-15');
