8a6f28b1147b21161b6dd779f7c801894ea9cccc
patorma <pocontrera@ing.ucsc.cl>
2020-04-13 00:04:39 -0400

    se agrego en backend lo de paginacion

--- src/main/java/com/patriciocontreras/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -10,6 +10,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -43,6 +46,11 @@ public class ClienteRestController {
 		return clienteService.findAll();
 	}
 
+	@GetMapping("/clientes/page/{page}")
+	public Page<Cliente> index(@PathVariable Integer page) {
+		  Pageable pageable = PageRequest.of(page, 4);
+		return clienteService.findAll(pageable);
+	}
 	@GetMapping("/clientes/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
 		Cliente cliente = null;
--- src/main/java/com/patriciocontreras/springboot/backend/apirest/models/services/ClienrteServiceImpl.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/models/services/ClienrteServiceImpl.java
@@ -3,6 +3,8 @@ package com.patriciocontreras.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,12 @@ public class ClienrteServiceImpl implements IClienteService {
 	public List<Cliente> findAll() {
 		
 		return (List<Cliente>) clienteDao.findAll();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
 	}
 
 	@Override
@@ -44,5 +52,7 @@ public class ClienrteServiceImpl implements IClienteService {
 		
 		clienteDao.deleteById(id);
 	}
+
+
 
 }
