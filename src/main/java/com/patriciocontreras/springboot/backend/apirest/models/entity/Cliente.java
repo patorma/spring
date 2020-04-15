626278114348daa2608cbefc41040d51fee2626a
patorma <pocontrera@ing.ucsc.cl>
2020-04-15 01:35:14 -0400

    se repitio registros para probar el funcionamiento de un paginador muy extenso

--- src/main/java/com/patriciocontreras/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/models/entity/Cliente.java
@@ -43,7 +43,7 @@ public class Cliente implements Serializable {
 	
 	@NotEmpty 
 	@Email
-	@Column(nullable = false, unique=true)
+	@Column(nullable = false, unique=false)
 	private String email;
 	
 	@Column(name="create_at")
