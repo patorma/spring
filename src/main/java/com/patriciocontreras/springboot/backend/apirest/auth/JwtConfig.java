cb9198e234cf302e99206b350a39851d6a4849df
patorma <pocontreras@ing.ucsc.cl>
2020-11-17 00:45:31 -0300

    se asigna la llave secreta Mac para firmar el token

---
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/auth/JwtConfig.java
@@ -0,0 +1,9 @@
+package com.patriciocontreras.springboot.backend.apirest.auth;
+
+public class JwtConfig {
+	//static se puede acceder desde la clase y no del objeto y 
+	//final no se puede cambiar el valor
+	
+	public static final String LLAVE_SECRETA = "alguna.clave.secreta.12345678";
+
+}
