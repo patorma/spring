77ccd84ad08c2f9b0cfd8bf8a59b9639c982d3db
patorma <pocontreras@ing.ucsc.cl>
2020-11-18 21:37:51 -0300

    Agregando seguridad a nuestras rutas de forma programatica con HttpSecurity

--- src/main/java/com/patriciocontreras/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/patriciocontreras/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -15,14 +15,21 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	// se implementa un solo metodo , que nos permite implementar todas las reglas de 
 	// seguridad de nuestros endpoints de nuestras rutas hacia los recursos 
 	// pero de nuestra aplicacion 
-	//.anyRequest().authenticated() siemrpe se coloca al final, para todas las rutas 
+	//.anyRequest().authenticated() siempre se coloca al final, para todas las rutas 
 	//(endpoints) que no hayamos asignado permiso
 	// Aqui se tienen reglas de acceso de usuarios por .oauth2
+	
+	//en el siguiente metodo se dan los permisos a los distintos tipos de usuarios
+	//a los recuroso endpoints segun su rol
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		//vamos a dar acceso al listado clientes(sin autorizacion)
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes","/api/clientes/page/**","/api/uploads/img/**").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
+		.antMatchers("/api/clientes/**").hasRole("ADMIN")
 		.anyRequest().authenticated();
 	}
 	
